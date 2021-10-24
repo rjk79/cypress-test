a021392155a96b37fa68e73bd8a7ead2d48eb07d
Robert Ku <robertku79@gmail.com>
2021-10-24 18:54:16 -0400

    Add cypress

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,20 @@
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+})
+
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+        cy.visit('https://example.cypress.io')
+        // cy.pause()
+        // cy.debug()
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
+    })
+})
